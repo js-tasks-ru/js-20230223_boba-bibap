1ec52192db8020dd42511ec67f01f161ae256e00
Nikita Kovalyov <boba.bibap@gmail.com>
2023-03-01 17:35:41 +0200

    finished with 2-nd hw

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -3,7 +3,20 @@
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
+ * 
  */
-export function sortStrings(arr, param = 'asc') {
+    function sortStrings(strings, order = 'asc') {
+         const locale = ['ru-u-kf-upper', 'en-US-u-kf-upper'];
 
+        if (order === 'asc') {
+            return strings.sort((a, b) => a.localeCompare(b, locale));
+        } else if (order === 'desc') {
+            return strings.sort((a, b) => b.localeCompare(a, locale));
+        }
 }
+
+
+sortStrings(['b', 'a', 'c'], 'asc'); // ['a', 'b', 'c']
+sortStrings(['b', 'a', 'c'], 'desc'); // ['c', 'b', 'a'] 
+sortStrings(['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'], 'asc');
+
